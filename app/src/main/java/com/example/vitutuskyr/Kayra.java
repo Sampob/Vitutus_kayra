adf0e93fc35a39c041f756b391d48764b885d191
sampob <sampo.bredenberg@metropolia.fi>
2020-12-03 14:22:03 +0200

    Base Päänäkymä ja käyrä aluilla

--- app/src/main/java/com/example/vitutuskyr/Kayra.java
+++ app/src/main/java/com/example/vitutuskyr/Kayra.java
@@ -2,13 +2,76 @@ package com.example.vitutuskyr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class Kayra extends AppCompatActivity {
+
+    private LineChart mpLineChart;
+    private ArrayList<Merkinta> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayra);
+
+        mpLineChart = findViewById(R.id.lineChart);
+        mpLineChart.setTouchEnabled(true);
+        mpLineChart.setPinchZoom(true);
+
+        readFile();
+
+        LineDataSet lineDataSet = new LineDataSet(dataValues(), "Käyrä");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet);
+
+        LineData data = new LineData(dataSets);
+        mpLineChart.setData(data);
+        mpLineChart.invalidate();
+
+        Description description = new Description();
+        description.setText("");
+        mpLineChart.setDescription(description);
+
+        lineDataSet.setLineWidth(4);
+        lineDataSet.setColor(Color.GRAY);
+        lineDataSet.setCircleColor(Color.BLUE);
+        lineDataSet.setDrawCircleHole(false);
+        lineDataSet.setCircleRadius(4);
+        lineDataSet.setValueTextSize(16);
+
+    }
+
+    public void readFile() {
+        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("lista", null);
+        Type type = new TypeToken<ArrayList<Merkinta>>() {}.getType();
+        lista = gson.fromJson(json, type);
+
+        if(lista == null) {
+            lista = new ArrayList<>();
+        }
+    }
+
+    private ArrayList<Entry> dataValues(){
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        for(int i = 0; i < lista.size(); i++){
+            dataVals.add(new Entry(i, lista.get(i).getNumero()));
+        }
+        return dataVals;
     }
 }
--- app/src/main/java/com/example/vitutuskyr/MainActivity.java
+++ app/src/main/java/com/example/vitutuskyr/MainActivity.java
@@ -3,30 +3,79 @@ package com.example.vitutuskyr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private NumberPicker picker;
-    private String[] pickerNums;
-    private EditText textBox;
+    public static final String EXTRA = "com.example.firstapp.test";
+
+    private ArrayList<Merkinta> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        picker = findViewById(R.id.numberPicker);
-        String[] pickerNums = new String[] {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
-        picker.setMinValue(0);
-        picker.setMaxValue(pickerNums.length-1);
-        picker.setWrapSelectorWheel(false);
+        readFile();
 
+        picker = findViewById(R.id.numberPicker);
+
+        String[] pickerNums = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        picker.setMinValue(1);
+        picker.setMaxValue(pickerNums.length);
+        picker.setWrapSelectorWheel(true);
         picker.setDisplayedValues(pickerNums);
+
+    }
+
+    public String getMessage(){
+        EditText editText = findViewById(R.id.editText);
+        return editText.getText().toString();
+    }
+
+    public int getNumber(){
+        return picker.getValue();
+    }
+
+    public void saveThings(View v) {
+        lista.add(new Merkinta(getMessage(), getNumber()));
+        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(lista);
+        editor.putString("lista", json);
+        editor.apply();
+
+        Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void readFile() {
+        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("lista", null);
+        Type type = new TypeToken<ArrayList<Merkinta>>() {}.getType();
+        lista = gson.fromJson(json, type);
+
+        if(lista == null) {
+            lista = new ArrayList<>();
+        }
+    }
+
+    public void graphView(View v){
+        Intent intent = new Intent(this, Kayra.class);
+        startActivity(intent);
     }
 
 }
