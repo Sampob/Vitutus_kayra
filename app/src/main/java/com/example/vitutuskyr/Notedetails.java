0ae4314d1d7ba4c8ddda5cbdd025dea0017c715d
antti-riiko <riikonen.j.antti@gmail.com>
2020-12-10 01:18:02 +0200

    antti

--- app/src/main/java/com/example/vitutuskyr/Notedetails.java
+++ app/src/main/java/com/example/vitutuskyr/Notedetails.java
@@ -2,14 +2,21 @@ package com.example.vitutuskyr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Notedetails extends AppCompatActivity {
 
     TextView textnote, textka, textday;
+    private ArrayList<Merkinta> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,20 @@ public class Notedetails extends AppCompatActivity {
         textka = findViewById(R.id.textKa);
         textday = findViewById(R.id.textDay);
 
+        readFile();
+        textnote.setText(lista.get(i).getNote());
+        textka.setText("" + lista.get(i).getNumero());
+        textday.setText("Päivä: " + lista.get(i).getCalendar().getTime());
+    }
+    private void readFile() {
+        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("lista", null);
+        Type type = new TypeToken<ArrayList<Merkinta>>() {}.getType();
+        lista = gson.fromJson(json, type);
 
+        if (lista == null){
+            lista = new ArrayList<>();
+        }
     }
 }
